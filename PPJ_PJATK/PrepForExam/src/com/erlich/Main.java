5b80b77ee26cc2947aae092bce9ba499d1e35b50
Dominik <dominik@dominik.home>
2020-02-07 12:11:08 +0100

    change Main.java in PrerForExam

--- PPJ_PJATK/PrepForExam/src/com/erlich/Main.java
+++ PPJ_PJATK/PrepForExam/src/com/erlich/Main.java
@@ -6,7 +6,8 @@ public class Main {
 //        drawSquare(5);
 //        drawShape(5);
         drawPyramid(7);
-        System.out.println("Hello world");
+        System.out.println("Hello world 123");
+
     }
 
     static void drawSquare(int n) {
